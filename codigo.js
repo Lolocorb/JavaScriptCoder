1f293c768bf61484d84c8474f3b6d2bd6bdb3cdc
lolo_corb <lorenzocorbalancabanillas@gmail.com>
2022-07-28 18:13:54 -0300

    lol

---
+++ codigo.js
@@ -0,0 +1,11 @@
+//declaracion de variables
+let equipofavorito;
+
+//inicializacion de la variable
+equipofavorito="Ajax"
+
+//declaracion e inicializacion de variables 
+let cantidadTitulos=15;
+
+//declaracion e inicializacion de constantes
+const PI=3.1416;
