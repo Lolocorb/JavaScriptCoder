94769a6b4fbc1b86c5dc49ab72bd0ccd97cc7f44
lolo_corb <lorenzocorbalancabanillas@gmail.com>
2022-08-01 20:46:27 -0300

    Ciclos

--- ciclos.js
+++ ciclos.js
@@ -1,14 +1,14 @@
-////ciclos por conteo -FOR
+//ciclos por conteo -FOR
 //for(let i=1; i<11;i++){
 //    console.log("Hola coders, ciclo "+i);
 //}
 
 
 
-////diapo17
-////solicitamos un valor al ususario
+//diapo17
+//solicitamos un valor al ususario
 //let ingresarNumero=parseInt(prompt("Ingresar Numero"))
-////En cada repeticion, calculamos el numero ingresado x el numero de repeticiones
+//En cada repeticion, calculamos el numero ingresado x el numero de repeticiones
 //for(let i = 1; i<= 10; i++) {
 //    let resultado = ingresarNumero * i ; 
 //    console.log(ingresarNumero +" X "+ i +" = "+ resultado);
@@ -17,8 +17,22 @@
 
 
 //diapo 18
-for(let i =1; i <=4; i ++) {
-    let ingresarNombre= prompt ("Ingresar Nombre");
+//for(let i =1; i <=4; i ++) {
+//   let ingresarNombre= prompt ("Ingresar Nombre");
 
-    console.log("Turno Numero "+i+" Nombre: "+ ingresarNombre);
+//   console.log("Turno Numero "+i+" Nombre: "+ ingresarNombre);
+// }
+
+
+//Sentencia break
+let password;
+for(let i=1; i<=3;i++){
+    password=prompt("Ingrese la constrasenia");
+     if(password=="coder123"){
+        console.log("Bienvenido usuario");
+        break;
+     }else{
+        console.log("Clave Erronea, quedan "+ (3-i)+" intentos");
+     }
 }
+
