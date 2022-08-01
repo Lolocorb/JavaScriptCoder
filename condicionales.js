8338c2dc8f1fcb4b8892b8ba5469c7938b582ace
lolo_corb <lorenzocorbalancabanillas@gmail.com>
2022-08-01 17:37:08 -0300

    Te amo messi

---
+++ condicionales.js
@@ -0,0 +1,54 @@
+//condicional simple
+// let gustoHelado=prompt("Ingresa tu gusto de helado favorito");
+
+// if(gustoHelado== "menta granizada"){
+//     console.log("Te ganaste 1/2kg de helado");
+// }else{
+//     console.log("Buena eleccion "+gustoHelado+" , lamentablemente no ganaste nada gil");
+// }
+
+
+//Ejercicio: Preguntar al ususario el anio de su vehiculo
+//Si el vehiculo tiene > de 10 anios de antiguedad le avisamos que abona 200usd de patente anual
+//Sino el vehiculo abona 100usd de patente
+
+
+
+// const ANIOACTUAL=2022;
+// let aniodevehiculo=parseInt(prompt ("Ingresa el anio del vehiculo"));
+// let antiguedad=ANIOACTUAL - aniodevehiculo;
+
+// if (antiguedad <10){
+//     console.log("abona 200usd de patente");
+// } else{
+//     console.log("abona 100usd de patente")
+// }
+
+let puntos=0;
+let banda=prompt("Cual es la banda que estabamos escuchando en la clase");
+let cancion=prompt("Cual es la ultima cancion de esa banda");
+
+if((banda=="daft punk") && (cancion=="game of love")){
+    console.log("Excelente ganaste 2 puntos");
+     puntos=puntos +2;
+}else if ((banda=="daft punk")&&(cancion!="fame of love")){
+    console.log("Bien la banda, mal la cancion. sumaste 1 punto");
+    puntos=puntos + 1 ;
+}else if ((banda!= "daft punk")&&(cancion=="game of love")){
+    console.log("Bien la cancion, mal la banda. Sumaste 1 punto");
+}else{
+    console.log("Error en ambas preguntas, no sumas puntos")
+}
+
+//pregunta2
+let nombreMessi=prompt("Ingresa el primer nombre de Messi");
+
+if((nombreMessi=="Lionel")|| (nombreMessi=="lionel")||(nombreMessi=="LIONEL")){
+    console.log("Excelente ganaste 2 puntos");
+     puntos=puntos +2;
+}else{
+    console.log("Error no sumas puntos");
+}
+
+
+console.log("Fin Del Juego-Puntos: "+puntos);
