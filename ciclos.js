4268b0d82936139984e77a03443a09e768d3c5eb
lolo_corb <lorenzocorbalancabanillas@gmail.com>
2022-08-01 21:25:28 -0300

    ciclos.js

--- ciclos.js
+++ ciclos.js
@@ -25,14 +25,34 @@
 
 
 //Sentencia break
-let password;
-for(let i=1; i<=3;i++){
-    password=prompt("Ingrese la constrasenia");
-     if(password=="coder123"){
-        console.log("Bienvenido usuario");
-        break;
-     }else{
-        console.log("Clave Erronea, quedan "+ (3-i)+" intentos");
-     }
-}
+// let password;
+// for(let i=1; i<=3;i++){
+//     password=prompt("Ingrese la constrasenia");
+//      if(password=="coder123"){
+//         console.log("Bienvenido usuario");
+//         break;
+//      }else{
+//         console.log("Clave Erronea, quedan "+ (3-i)+" intentos");
+//      }
+// }
 
+
+
+//Sentencia continue
+// for(let i = 1; i <= 10; i ++) {
+//     if(i == 5){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+//While, Bucle!
+ let oracion="";
+ let palabra=prompt("Ingresa una palabra para armar una oracion (s-salir del juego) ");
+
+ while(palabra!="s"){
+    oracion = oracion + " " + palabra;
+    palabra=prompt("Ingresa otra palabra para armar la oracion (s-salir del juego)");
+ }
+
+ alert("La oracion formada es: "+oracion);
